6d9547e55e1cc973cf04732e00ef8a403def610c
Chihiro072 <bryan.pradibta@gmail.com>
2026-09-11 20:47:35 +0700

    Add Noto Sans SC variable font to the project

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  outputFileTracingIncludes: {
+    "/api/worksheet": ["./public/fonts/NotoSansSC-VF.ttf"],
+  },
 }
 
 export default nextConfig
